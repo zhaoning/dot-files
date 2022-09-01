8e986d1f3a51fd09c0bddd329a5f6d708bc430e3
Ning Zhao <zhaoning.zju@gmail.com>
2022-09-02 09:07:28 +1000

    Change date format in capture templates.

--- .emacs.d/init/org-capture.el
+++ .emacs.d/init/org-capture.el
@@ -1,21 +1,21 @@
 (setq org-capture-templates
   '(("t" "Task" entry (file "~/org/inbox.org")
-     "* TODO %?\n%U\n%a\n")
+     "* TODO %?\n%u\n%a\n")
     ("r" "Respond" entry (file "~/org/inbox.org")
-     "* NEXT Respond to %^{from} on %^{subject}\nSCHEDULED: %t\n%U\n%a\n"
+     "* NEXT Respond to %^{from} on %^{subject}\nSCHEDULED: %t\n%u\n%a\n"
      :immediate-finish t)
     ("n" "Note" entry (file "~/org/inbox.org")
-     "* %? :NOTE:\n%U\n%a\n")
+     "* %? :NOTE:\n%u\n%a\n")
     ("j" "Journal" entry (file+datetree "~/org/diary.org")
-     "* %?\n%U\n")
+     "* %?\n%u\n")
     ("i" "Idea" entry (file "~/org/inbox.org")
-     "* %? :IDEA:\n%U\n")
+     "* %? :IDEA:\n%u\n")
     ("m" "Meeting" entry (file "~/org/inbox.org")
-     "* MEETING %? :MEETING:\n%U\n"
+     "* MEETING %? :MEETING:\n%u\n"
      :clock-in t :clock-resume t)
     ("p" "Phone" entry (file "~/org/inbox.org")
-     "* PHONE %? :PHONE:\n%U\n"
+     "* PHONE %? :PHONE:\n%u\n"
      :clock-in t :clock-resume t)
     ("g" "Grocery" entry (file "~/org/inbox.org")
-     "* TODO %? :grocery:\n%U\n")
+     "* TODO %? :grocery:\n%u\n")
     ))
