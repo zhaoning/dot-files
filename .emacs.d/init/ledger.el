9524726a58c2cd95ab639f9fd3e25732769745a1
Ning Zhao <zhaoning.zju@gmail.com>
2022-10-11 20:55:33 +1100

    Ledger reports streamlined.

--- .emacs.d/init/ledger.el
+++ .emacs.d/init/ledger.el
@@ -3,20 +3,56 @@
 (setq ledger-schedule-file "schedule.ledger")
 
 (setq ledger-reports
-      '(("up" "%(binary) -f %(ledger-file) reg ^Liability:Payable --effective -b \"today\" -S \"date\"")
-        ("nw" "%(binary) -f %(ledger-file) bal -cnX AUD ^Asset ^Liability")
-;;	("Budget" "%(binary) -f %(ledger-file) bal --budget --effective")
-        ("mea" "%(binary) -f %(ledger-file) --period 'last 12 months' -M --effective reg %(account)")
-        ("bs" "%(binary) -f %(ledger-file) bal ^Asset ^Liability -c --no-total --depth 3")
-	("Register General" "%(binary) -f %(ledger-file) reg -c Asset:Receivable:Yan\\ Sun -F \"%D %-.25P %15t %-30N\\n\" -b 2022-09-11")
-        ("Register of Payee" "%(binary) -f %(ledger-file) reg -c @%(payee)")
-        ("Register of Account" "%(binary) -f %(ledger-file) reg -c %(account)")
-	("Pending" "%(binary) -f %(ledger-file) reg --pending")
-	("My Dining" "%(binary) -f %(ledger-file) -P reg ^Expense:Dining\\ Out")
-	("My Payees" "%(binary) -f %(ledger-file) -P reg -S T")
-        ("Account List" "%(binary) -f %(ledger-file) accounts")
-        ("FY22 Investment Activity Summary - Swyftx" "%(binary) -f %(ledger-file) -b 2021-07-01 -e 2022-07-01 reg ^Asset:Crypto:Swyftx:Current -P")
-        ("FY22 Investment Activity Summary - IG" "%(binary) -f %(ledger-file) -b 2021-07-01 -e 2022-07-01 reg ^Asset:Security:IG:Current -P")
-        ("FY22 Investment Allocation" "%(binary) -f %(ledger-file) -e 2022-06-30 bal --no-total ^Asset:Crypto ^Asset:Security ^Asset:Property -X AUD")
-        ("FY22 Income" "%(binary) -f %(ledger-file) -b 2021-07-01 -e 2022-07-01 bal ^Income -X AUD")
-        ("FY22 Expense" "%(binary) -f %(ledger-file) -b 2021-07-01 -e 2022-07-01 bal ^Expense -X AUD")))
+      '(;; RECONCILIATION
+        ("rga - Register of Account"
+	 "%(binary) -f %(ledger-file) reg %(account) -c")
+        ("rgp - Register of Payee"
+	 "%(binary) -f %(ledger-file) reg @%(payee) -c")
+	("pe - Pending"
+	 "%(binary) -f %(ledger-file) reg --pending")
+	;; EXECUTION
+	("up - Upcoming Payables"
+	 "%(binary) -f %(ledger-file) reg ^Liability:Payable --effective -b \"today\" -S \"date\"")
+	;; BUDGETING
+	("bb - Budget Balance"
+	 "%(binary) -f %(ledger-file) bal --budget --effective")
+        ("era - Effective Register of Account"
+	 "%(binary) -f %(ledger-file) reg %(account) --effective --period 'last 12 months' -M")
+        ("erp - Effective Register of Payee"
+	 "%(binary) -f %(ledger-file) reg @%(payee) --effective --period 'last 12 months' -M")
+	;; FORECASTING
+	;;
+	;; TAX
+	;;
+	;; PROGRESSION
+        ("nw - Net Worth"
+	 "%(binary) -f %(ledger-file) bal ^Asset ^Liability -cnX AUD")
+        ("bs3 - Balance Sheet D3"
+	 "%(binary) -f %(ledger-file) bal ^Asset ^Liability -c --no-total --depth 3")
+        ("bsx - Balance Sheet AUD"
+	 "%(binary) -f %(ledger-file) bal ^Asset ^Liability -c -X AUD")
+        ("in22x - Income FY22 AUD"
+	 "%(binary) -f %(ledger-file) bal ^Income -b 2021-07-01 -e 2022-07-01 -X AUD")
+        ("ex22x - Expense FY22"
+	 "%(binary) -f %(ledger-file) bal ^Expense -b 2021-07-01 -e 2022-07-01 -X AUD")
+        ("pl22x - Profit Loss FY22"
+	 "%(binary) -f %(ledger-file) bal ^Income ^Expense -b 2021-07-01 -e 2022-07-01 -X AUD")
+	;; INVESTMENT
+        ("aa22x - Investment Allocation FY22"
+	 "%(binary) -f %(ledger-file) bal ^Asset:Crypto ^Asset:Security ^Asset:Property -e 2022-06-30 -X AUD")
+	;; INFORMATION
+        ("acl - Account List"
+	 "%(binary) -f %(ledger-file) accounts")
+	("pyl - Payee List"
+	 "%(binary) -f %(ledger-file) payees")
+	("pyr - Payee Ranking"
+	 "%(binary) -f %(ledger-file) reg -P -S T")
+	("pyd - Payee Dining"
+	 "%(binary) -f %(ledger-file) reg ^Expense:Dining\\ Out -P -S T")
+	("pyg - Payee Groceries"
+	 "%(binary) -f %(ledger-file) reg ^Expense:Groceries -P -S T")
+	;; OPEN
+	("rg - Register"
+	 "%(binary) -f %(ledger-file) reg Asset:Receivable:Yan\\ Sun -c -F \"%D %-.25P %15t %-30N\\n\" -b 2022-09-11")
+	;; UNSORTED
+	))
